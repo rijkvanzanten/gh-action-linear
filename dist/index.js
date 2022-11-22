e47248dc87466a89f373d091856e7f1ac66981a8
rijkvanzanten <rijkvanzanten@me.com>
2022-11-22 17:39:01 -0500

    Debug push

--- dist/index.js
+++ dist/index.js
@@ -9534,6 +9534,7 @@ const findLinearComment = async (octokit, { issue, repo }) => {
     });
     const linearIdRegex = /<!-- linear-issue-id: \[([0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})\] -->/gm;
     const linearComment = comments.data.find((comment) => {
+        console.log(comment);
         return (comment.user?.login === "github-actions" &&
             linearIdRegex.test(comment.body_text ?? ""));
     });
--- src/find-linear-comment.ts
+++ src/find-linear-comment.ts
@@ -14,6 +14,7 @@ export const findLinearComment = async (
 		/<!-- linear-issue-id: \[([0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})\] -->/gm;
 
 	const linearComment = comments.data.find((comment) => {
+		console.log(comment);
 		return (
 			comment.user?.login === "github-actions" &&
 			linearIdRegex.test(comment.body_text ?? "")
