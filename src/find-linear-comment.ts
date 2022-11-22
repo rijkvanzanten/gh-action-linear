772d220f31e63f0b76ea41b0729eb4e81c259768
rijkvanzanten <rijkvanzanten@me.com>
2022-11-22 17:42:08 -0500

    Don't think body exists

--- src/find-linear-comment.ts
+++ src/find-linear-comment.ts
@@ -14,15 +14,12 @@ export const findLinearComment = async (
 		/<!-- linear-issue-id: \[([0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})\] -->/gm;
 
 	const linearComment = comments.data.find((comment) => {
-		console.log(comment);
 		return (
 			comment.user?.login === "github-actions[bot]" &&
 			comment.user?.type === "Bot" &&
-			linearIdRegex.test(comment.body_text ?? "")
+			linearIdRegex.test(comment.body ?? "")
 		);
 	});
-
-	console.log(linearComment);
 
 	if (!linearComment) {
 		throw new Error(`Couldn't extract linear comment for issue #${issue}.`);
