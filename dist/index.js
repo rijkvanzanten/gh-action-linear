e3a00bcc9cb725e883876baf46a85ccf2592da51
rijkvanzanten <rijkvanzanten@me.com>
2022-12-03 17:07:35 -0500

    Build

--- dist/index.js
+++ dist/index.js
@@ -9506,24 +9506,21 @@ const createLinearComment = async (linear, { linearIssueId, linearIssueBody }) =
 /* harmony export */ __nccwpck_require__.d(__webpack_exports__, {
 /* harmony export */   "D": () => (/* binding */ findLinearComment)
 /* harmony export */ });
-const findLinearComment = async (octokit, { githubIssueNumber, githubRepo }) => {
+const findLinearComment = async (octokit, { githubIssueNumber, githubRepo, }) => {
     const comments = await octokit.rest.issues.listComments({
         ...githubRepo,
         issue_number: githubIssueNumber,
         per_page: 100,
     });
-    const uuidRegex = /[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/;
+    const uuidRegex = /linear-issue-id: \[([0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})\]/;
     const linearComment = comments.data.find((comment) => {
-        return (comment.user?.login === "github-actions[bot]" &&
-            comment.user?.type === "Bot" &&
-            comment.body &&
-            uuidRegex.test(comment.body));
+        return comment.body && uuidRegex.test(comment.body);
     });
     if (!linearComment) {
         throw new Error(`Couldn't extract Linear comment on issue #${githubIssueNumber}`);
     }
     // We know it exists as we're testing for it in the find call above
-    return linearComment.body.match(uuidRegex)[0];
+    return linearComment.body.match(uuidRegex)[1];
 };
 
 
